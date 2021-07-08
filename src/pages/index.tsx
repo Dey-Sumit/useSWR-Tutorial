322f669f59ce35326c8f6eee507fee346366df10
unknown <sumax333@gmail.com>
2021-07-08 16:21:00 +0530

    added pagiantion in swr with custom hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,19 @@ import PostCard from "@components/PostCard";
 import Loader from "@components/Loader";
 import { IPost } from "@libs/types";
 
-import useSWR from "swr";
+import { useSWRInfinite } from "swr";
+import { usePagination } from "@libs/usePagination";
 
 export default function Home() {
   //! 4 👇
-  const { data: posts, error } = useSWR<IPost[]>(`/posts`);
+  // const { data: posts, error } = useSWR<IPost[]>(`/posts`);
+  const {
+    paginatedData: paginatedPosts,
+    page,
+    setPage,
+    isLoadingMore,
+    isReachedEnd,
+  } = usePagination("/posts");
 
   // //! 1 👇
   // const [posts, setPosts] = useState<IPost[]>();
@@ -29,11 +37,22 @@ export default function Home() {
 
       <h4>Posts</h4>
 
-      {!posts && <Loader />}
-      {error && <h4>something went wrong</h4>}
-      {posts?.map((post, i) => (
+      {/* {error && <h4>something went wrong</h4>} */}
+      {paginatedPosts?.map((post, i) => (
         <PostCard key={i} post={post} />
       ))}
+      {/* {!posts && <Loader />} */}
+      {!paginatedPosts.length && <Loader />}
+
+      {isLoadingMore && <Loader />}
+      {!!paginatedPosts.length && !isReachedEnd && (
+        <button
+          onClick={() => setPage(page + 1)}
+          className=" btn btn-outline-warning d-block mx-auto"
+        >
+          Load More
+        </button>
+      )}
     </div>
   );
 }
--- src/pages/posts/[postId].tsx
+++ src/pages/posts/[postId].tsx
@@ -5,6 +5,7 @@ import Loader from "@components/Loader";
 import { IComment, IPost } from "@libs/types";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import { usePagination } from "@libs/usePagination";
 
 const index = () => {
   const {
@@ -12,12 +13,20 @@ const index = () => {
   } = useRouter();
 
   //! 5 👇
-  const { data: comments, error: commentsError } = useSWR<IComment[]>(
-    postId && `/posts/${postId}/comments`
-  );
+  // const { data: comments, error: commentsError } = useSWR<IComment[]>(
+  //   postId && `/posts/${postId}/comments`
+  // );
 
   //! 7 👇
   const { data: posts, error: postsError } = useSWR<IPost[]>(postId && `/posts`);
+
+  const {
+    paginatedData: paginatedComments,
+    page,
+    setPage,
+    isLoadingMore,
+    isReachedEnd,
+  } = usePagination(`/posts/${postId}/comments`);
 
   // const [comments, setComments] = useState<IComment[]>();
   // const [post, setPost] = useState<IPost>();
@@ -49,15 +58,23 @@ const index = () => {
     <div>
       {posts ? <PostCard post={posts[postIndex]} /> : <Loader />}
 
-      <CreateComment postId={postId}/>
+      <CreateComment postId={postId} />
 
       <h4>Comments</h4>
 
-      {!comments && <Loader />}
-
-      {comments?.map((comment, i) => (
+      {paginatedComments?.map((comment, i) => (
         <CommentCard key={i} comment={comment} />
       ))}
+      {!paginatedComments.length && <Loader />}
+      {isLoadingMore && <Loader />}
+      {paginatedComments.length && !isReachedEnd && (
+        <button
+          onClick={() => setPage(page + 1)}
+          className=" btn btn-outline-warning d-block mx-auto"
+        >
+          Load More
+        </button>
+      )}
     </div>
   );
 };
