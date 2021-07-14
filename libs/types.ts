e962c4c99299b384862eb5543e408a8b6eb4c7c9
Sumit Dey <46570780+Dey-Sumit@users.noreply.github.com>
2021-07-14 17:45:03 +0530

    Update types.ts

--- libs/types.ts
+++ libs/types.ts
@@ -1,9 +1,11 @@
 export interface IPost {
   content: string;
   id: number;
+  createdAt:Date
 }
 export interface IComment {
   content: string;
   id: number;
   postId: number;
+  createdAt:Date
 }
